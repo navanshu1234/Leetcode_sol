e7507ff6a85f20f4fcfb999aa3a2f5d1a964da0d
Navanshu <140784946+navanshu1234@users.noreply.github.com>
2023-12-22 12:28:32 +0530

    Update 15: 3Sum.java

--- leetcode_solutions/15: 3Sum.java
+++ leetcode_solutions/15: 3Sum.java
@@ -36,4 +36,28 @@ class Solution {
 } 
 // time limit exceed
 
+/*python code:
+class Solution(object):
+    def threeSum(self, nums):
+        ans = []
+        nums.sort()
+        n = len(nums)
+        sum_val = 0
+        x = 0
 
+        for i in range(n):
+            for j in range(1, n):
+                for k in range(n):
+                    if i != j and j != k and k != i:
+                        sum_val = nums[i] + nums[j] + nums[k]
+                        if sum_val == 0:
+                            ans1 = [nums[i], nums[j], nums[k]]
+                            ans1.sort()
+                            if ans1 not in ans:
+                                ans.append(ans1)
+                                x -= 1
+
+        return ans
+        
+*/
+// time limit exceed case 286
