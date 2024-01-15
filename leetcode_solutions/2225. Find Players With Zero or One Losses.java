26882b7f77f5048ac58fa7cb36897df7cfc53d05
Navanshu <140784946+navanshu1234@users.noreply.github.com>
2024-01-15 22:31:53 +0530

    Update 2225. Find Players With Zero or One Losses.java

--- leetcode_solutions/2225. Find Players With Zero or One Losses.java
+++ leetcode_solutions/2225. Find Players With Zero or One Losses.java
@@ -34,3 +34,44 @@ class Solution {
         return ar;
     }
 }
+// time limit exceed
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> ar = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        ArrayList<Integer> ar1 = new ArrayList<Integer>();
+        int n = matches.length;
+        
+        for(int i=0;i<n;i++)
+        {   boolean t = true;
+            boolean t1 = true;
+            for(int j=0;j<n;j++)
+            {
+                if(matches[i][0]==matches[j][1])
+                {
+                    t = false;
+                }
+                if(matches[i][1]==matches[j][1] && i!=j)
+                {
+                    t1 = false;
+                }
+            }
+            if(t==true && !arr.contains(matches[i][0]))
+            {
+                arr.add(matches[i][0]);
+            }
+            if(t1==true && !ar1.contains(matches[i][0]))
+            {
+                ar1.add(matches[i][1]);
+            }
+        }
+        Collections.sort(ar1);
+        Collections.sort(arr);
+        ar.add(arr);
+        ar.add(ar1);
+        
+        
+        return ar;
+    }
+}
+// time limit exceed
