362f6848a33d2177aa14bc6559be2c9b0d17962a
Navanshu <140784946+navanshu1234@users.noreply.github.com>
2024-01-20 22:28:58 +0530

    Update 907. Sum of Subarray Minimums.java

--- leetcode_solutions/907. Sum of Subarray Minimums.java
+++ leetcode_solutions/907. Sum of Subarray Minimums.java
@@ -36,31 +36,61 @@ class Solution {
 }
 //time limit exceed case: 72
 class Solution {
-    public int sumSubarrayMins(int[] arr) {
-       int n = arr.length;
-       int sum = 0;
-        for(int i=0;i<n;i++)
-        {   ArrayList<Integer> ar = new ArrayList<Integer>();
-            for(int j=i;j<n;j++)
-            {   
-               ar.add(j,arr[j]);
-                min = ar.get(0);
-               if(ar.size()>1)
-               {
-                   if(ar.get(j)<min))
-                   {
-                       min = ar.get(j);
-                   }
-               }
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;
 
-               sum = sum + min;
+        for (int i = 0; i < n; i++) {
+            std::vector<int> subarray;
+            for (int j = i; j < n; j++) {
+                subarray.push_back(arr[j]);
+                int min_val = *std::min_element(subarray.begin(), subarray.end());
+                sum = (sum + min_val) % ((int)pow(10, 9) + 7);
             }
         }
-        int mod =  (int)Math.pow(10,9) + 7;
-        return sum%mod;
+
+        return sum;
     }
-}
+}; // Yes, it is equivalent c++ solution, time limit exceed case: 77
+// Here comes the python solution
+class Solution(object):
+    def sumSubarrayMins(self, arr):
+        n = len(arr)
+        mod = 10**9 + 7
+        total_sum = 0
+
+        for i in range(n):
+            subarray = []
+            for j in range(i, n):
+                subarray.append(arr[j])
+                min_val = min(subarray)
+                total_sum = (total_sum + min_val) % mod
+
+        return total_sum
+//time limit exceed case: 77
+// here comes C code:
+    int sumSubarrayMins(int* arr, int arrSize) {
+    int mod = 1000000007;
+    int sum = 0;
+    int n = arrSize;
+    for (int i = 0; i < n; i++) {
+        int subarray[n - i];
+        for (int j = i; j < n; j++) {
+            subarray[j - i] = arr[j];
+            int min_val = subarray[0];
+            for (int k = 1; k <= j - i; k++) {
+                if (subarray[k] < min_val) {
+                    min_val = subarray[k];
+                }
+            }
+            sum = (sum + min_val) % mod;
+        }
+    }
+
+    return sum;
+} //time limit exceed case: 77
 /* Another approach:
     find all subarray(contigous) (the subarray division is similar to all sub string finding of a string, You can also try this question by converting given array to string and divide them into substring using s.substring(i,j+1) to get all contigous substring), size= (n*(n+1))/2 and add to arraylist which is added to list of arraylist
     then find all min in List and add them;
-    then divide by modulo 
+    then divide by modulo */
