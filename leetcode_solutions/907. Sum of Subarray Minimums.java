034e95668282cfb3284d1c42790b6a564ab66f0a
Navanshu <140784946+navanshu1234@users.noreply.github.com>
2024-01-20 21:23:27 +0530

    Update 907. Sum of Subarray Minimums.java

--- leetcode_solutions/907. Sum of Subarray Minimums.java
+++ leetcode_solutions/907. Sum of Subarray Minimums.java
@@ -16,3 +16,22 @@ class Solution {
     }
 }
 //time limit exceed
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+       int n = arr.length;
+       int sum = 0;
+        for(int i=0;i<n;i++)
+        {   ArrayList<Integer> ar = new ArrayList<Integer>();
+            for(int j=i;j<n;j++)
+            {   
+               ar.add(arr[j]);
+               Collections.sort(ar);
+               int min = ar.get(0);
+               sum = sum + min;
+            }
+        }
+        int mod =  (int)Math.pow(10,9) + 7;
+        return sum%mod;
+    }
+}
+//time limit exceed case: 72
