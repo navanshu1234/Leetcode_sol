16d6f5cc52a4f8d76c1c5a406ec57d62977760ee
Navanshu <140784946+navanshu1234@users.noreply.github.com>
2024-02-05 21:33:19 +0530

    Update 387. First Unique Character in a String.java

--- leetcode_solutions/387. First Unique Character in a String.java
+++ leetcode_solutions/387. First Unique Character in a String.java
@@ -24,3 +24,23 @@ class Solution {
     }
 }
 //timelimit exceed
+class Solution {
+    public int firstUniqChar(String s) {
+        int ans =-1;
+        char c[] =  s.toCharArray();
+        int count[] = new int[26];
+        for(int i=0;i<c.length;i++)
+        {   
+            ++count[c[i]-'a'];
+        }
+        for(int i=0;i<c.length;i++)
+        {   
+            if(count[c[i]-'a'] == 1)
+       {
+           ans =i ;
+           break;
+       }
+        }
+        return ans;
+    }
+}
